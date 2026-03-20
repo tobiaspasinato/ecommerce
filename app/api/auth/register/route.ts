726e30649e8b25702aaee9257eb00a8f3b333243
tobiaspasinato <tobiasmarpasinato@gmail.com>
2026-03-20 10:41:11 -0300

    feat(api): implement authentication endpoints for login, registration, and user retrieval

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from 'next/server';
+import dotenv from 'dotenv';
+import pool from '@/lib/db';
+import bcrypt from 'bcryptjs';
+import { RowDataPacket, FieldPacket } from 'mysql2/promise';
+
+dotenv.config();
+
+export async function GET() {
+    const [rows] = await pool.query('SELECT * FROM usuarios') as [RowDataPacket[], FieldPacket[]];
+    return NextResponse.json(rows);
+}
+
+export async function POST(request: NextRequest) {
+    try {
+        const { email, password } = await request.json();
+        
+        // Check if user already exists
+        const [existingUser] = await pool.query('SELECT * FROM usuarios WHERE email = ?', [email]) as [RowDataPacket[], FieldPacket[]];
+        if (existingUser.length > 0) {
+            return NextResponse.json({ message: 'User already exists' }, { status: 400 });
+        }
+        
+            // Hash the password
+            const hashedPassword = await bcrypt.hash(password, 10);
+        
+            // Insert new user into the database
+            await pool.query('INSERT INTO usuarios (email, password_hash) VALUES (?, ?)', [email, hashedPassword]);
+        
+        return NextResponse.json({ 
+            message: `Usuario ${email} registrado exitosamente!` 
+        }, { status: 201 });
+    } catch (error) {
+        console.error('Error en registro:', error);
+        return NextResponse.json(
+            { error: 'Algo salió mal' },
+            { status: 500 }
+        );
+    }
+}
